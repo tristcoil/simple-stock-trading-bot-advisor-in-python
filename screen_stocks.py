048bc9770169050c101d3c2a765255aacae91509
tristcoil <michal.vasulka@gmail.com>
2020-09-18 22:04:00 +0200

    added support for moving averages

--- screen_stocks.py
+++ screen_stocks.py
@@ -1,5 +1,6 @@
 #!/home/coil/anaconda3/bin/python3
 
+
 #my program to run on google free tier VM
 # stock screener
 
@@ -13,6 +14,22 @@
 # or maybe
 #!pip3 install yfinance --upgrade --no-cache-dir
 #!pip3 install pandas_datareader
+
+
+
+# create lock file quickly to prevent cron spinning infinite number of
+# script instances
+import sys
+import os
+
+lockfile = '/home/coil/scripts/script.lock'
+if not os.path.exists(lockfile):
+    with open(lockfile, 'w'): pass
+else:
+    print('lockfile exists, exiting')
+    sys.exit(1)
+
+
 
 # ___library_import_statements___
 import pandas as pd
@@ -35,23 +52,37 @@ import yfinance as yahoo_finance
 import smtplib
 from email.mime.text import MIMEText
 
+
+
 #                           ___variables___
+# ------------------------------------------------------------------------------
 
-tickers = ['AAPL','AMZN', 'GOOG', 'FB', 'MSFT', 'BPY', 'XOM']
+#tickers = ['AAPL','AMZN', 'GOOG', 'FB', 'MSFT', 'BPY', 'XOM']
+#ticker = 'AAPL'
 
-ticker = 'AAPL'
+with open('/home/coil/scripts/stock_list.txt', 'r') as f:
+    # list in following format
+    # MMM 3M Company  Industrials Industrial Conglomerates
+    #tickers = [f.read().splitlines()[0] for line in f]
+    tickers = [line.split()[0] for line in f]
+
 
 start_time = datetime.datetime(2017, 10, 1)
 #end_time = datetime.datetime(2019, 1, 20)
 end_time = datetime.datetime.now().date().isoformat()     # today
 
 username = 'coin.market.cap.000@gmail.com'
-password = 'putyourpasswordhere'
+password = ''
+
+
+
 
 #                       __function_definitions__
+# ------------------------------------------------------------------------------
 
 def get_data(ticker):
     # yahoo gives only daily historical data
+    attempts = 0
     connected = False
     while not connected:
         try:
@@ -61,6 +92,9 @@ def get_data(ticker):
         except Exception as e:
             print("type error: " + str(e))
             time.sleep( 5 )
+            attempts += 1
+            if attempts >= 10:
+                connected = True
             pass
 
     # use numerical integer index instead of date
@@ -94,7 +128,34 @@ def computeRSI (data, time_window):
     rsi = 100 - 100/(1+rs)
     return rsi
 
-def send_email(data, username, password):
+
+def computeSMA(data, window):
+    # simple moving average
+    sma = data.rolling(window=window).mean()
+    return sma
+
+def computeEMA(data, span):
+    # simple moving average
+    ema = data.ewm(span=span, adjust=False).mean()
+    return ema
+
+
+def construct_df(ticker):
+    #get data from yahoo API
+    df = get_data(ticker)
+    # compute both types of moving averages
+    for i in range(50, 250, 50):
+        #print(i)
+        df['SMA_{}'.format(i)] = computeSMA(df['Adj Close'], i)
+    for i in range(50, 250, 50):
+        #print(i)
+        df['EMA_{}'.format(i)] = computeEMA(df['Adj Close'], i)
+
+    return df
+
+
+
+def send_email(data_rsi, data_ema, username, password):
 
     smtp_ssl_host = 'smtp.gmail.com'
     smtp_ssl_port = 465
@@ -102,16 +163,25 @@ def send_email(data, username, password):
     receiver = 'michal.vasulka@yahoo.com'
 
     # implicitly joined string
-    msg_body = ("stock ticker RSI <= 30 \n"
+    msg_body_rsi = ("stock ticker RSI around 30 \n"
                 "possible long entry \n"
                 "ticker/s: \n"
-                 + data)
+                 + data_rsi + "\n")
+
+    msg_body_ema = ("went above 200 EMA recently \n"
+                "possible long entry \n"
+                "ticker/s: \n"
+                 + data_ema)
+
+    msg_body = msg_body_rsi + msg_body_ema
+
 
     message = MIMEText(msg_body, "plain")
     # treat message as dictionary
     message['subject'] = 'stock event'
     message['from']    = sender
     message['to']      = receiver
+
 
     # contact gmail server and send mail via my gmail dummy account
     try:
@@ -123,21 +193,56 @@ def send_email(data, username, password):
     except:
         print("Error: unable to send email")
 
-##                     __main_code_part__
-long_list = []
+
+
+##                          __main_code_part__
+# ------------------------------------------------------------------------------
+
+rsi_signal = []
+ema_signal = []
 
 for ticker in tickers:
-    df = get_data(ticker)
-    #adds RSI column to dataframe
-    df['RSI'] = computeRSI(df['Adj Close'], 14)
-    # RSI <= 30 is long signal
-    # if last day RSI data (today) is oversold, send mail
-    print('ticker:', ticker)
-    print('rsi today', df['RSI'].iloc[-1])
+    try:
+        # df = get_data(ticker)       #just gets data
+        df = construct_df(ticker)     #gets data and adds MAs to the df (implement RSI later)
+        #adds RSI column to dataframe
+        df['RSI'] = computeRSI(df['Adj Close'], 14)
+        # RSI <= 30 is long signal
+        # if last day RSI data (today) is oversold, send mail
+        print('ticker:', ticker)
+        print('rsi today', df['RSI'].iloc[-1])
 
-    if df['RSI'].iloc[-1] <= 30:
-        long_list.append(ticker)
+        ## RSI day before <= threshold and RSI today above - long signal
+        ##if (df['RSI'].iloc[-2] < 30 and df['RSI'].iloc[-1] >= 30):
+        ##    long_list.append(ticker)
 
-if len(long_list) > 0:
-    long_str = ' '.join(map(str, long_list))
-    send_email(long_str, username, password)
+        #s __signal_conditions__
+        if (df['RSI'].iloc[-1] <= 30):
+            rsi_signal.append(ticker)
+
+        # was below 200 EMA few days ago but today is above 200 EMA
+        # possible long
+        if ( (df['EMA_200'].iloc[-5] > df['Adj Close'].iloc[-5]) and (df['EMA_200'].iloc[-1] < df['Adj Close'].iloc[-1]) ):
+            ema_signal.append(ticker)
+
+
+    except Exception as e:
+        print("type error: " + str(e))
+
+
+
+# implement lists as dictionaries for clarity
+#signal = {}
+#signal['RSI'] = []
+#signal['SMA'] = []
+
+
+if ( len(rsi_signal) > 0 ) or ( len(ema_signal) > 0 ) :
+    rsi_str = ' '.join(map(str, rsi_signal))
+    ema_str = ' '.join(map(str, ema_signal))
+    send_email(rsi_str, ema_str, username, password)
+
+
+# lockfile cleanup
+os.remove(lockfile)
+
